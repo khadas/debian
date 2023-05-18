khadas/debian

commit 6cdba651c4a088d9995b3c6092b640b21b614f05

mk-rootfs-bullseye.sh: Fixup owners
Change-Id: I794e287c9eba04ff0f68e67d1969ff9f59f9c848
Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

mk-rootfs-bullseye.sh
@@ -79,6 +79,12 @@ sudo mount -o bind /dev $TARGET_ROOTFS_DIR/dev
 
 cat << EOF | sudo chroot $TARGET_ROOTFS_DIR
 
+# Fixup owners
+find / -user $(id -u) -exec chown -h -R 0:0 {} \;
+for u in \$(ls /home/); do
+	chown -h -R \$u:\$u /home/\$u
+done
+
 echo "deb http://mirrors.ustc.edu.cn/debian/ bullseye-backports main contrib non-free" >> /etc/apt/sources.list
 echo "deb-src http://mirrors.ustc.edu.cn/debian/ bullseye-backports main contrib non-free" >> /etc/apt/sources.list
 
@@ -242,7 +248,6 @@ cd -
 rm -rf /var/lib/apt/lists/*
 rm -rf /var/cache/
 rm -rf /packages/
-
 
 EOF
 

commit 7f921d1b5cccc1c8fea3a603e623f0f34f264ef7

mk-rootfs-bullseye.sh: Fixup owners with id=0
Fixes: 6cdba651c4a0 ("mk-rootfs-bullseye.sh: Fixup owners")

Change-Id: Ia9cc36efe1b53e68f6f48ea4af2a7ed295416a92
Signed-off-by: Caesar Wang <wxt@rock-chips.com>

mk-rootfs-bullseye.sh
@@ -68,10 +68,14 @@ sudo cp -f /etc/resolv.conf $TARGET_ROOTFS_DIR/etc/
 
 sudo mount -o bind /dev $TARGET_ROOTFS_DIR/dev
 
+ID=$(stat --format %u $TARGET_ROOTFS_DIR)
+
 cat << EOF | sudo chroot $TARGET_ROOTFS_DIR
 
 # Fixup owners
-find / -user $(id -u) -exec chown -h -R 0:0 {} \;
+if [ "$ID" -ne 0 ]; then
+       find / -user $ID -exec chown -h -R 0:0 {} \;
+fi
 for u in \$(ls /home/); do
 	chown -h -R \$u:\$u /home/\$u
 done