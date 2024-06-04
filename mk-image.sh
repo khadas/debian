0eac447b81b0a6499c33fba1ae27a2bcf9c2cd3a
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-06-04 16:32:17 +0800

    mk-image.sh: Improve auto-sizing
    Calculate the holes in sparse files as well.
    
    Change-Id: Ica406a3dac4f3aac767a88a3a8ccff929fca359f
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- mk-image.sh
+++ mk-image.sh
@@ -15,8 +15,13 @@ for script in ./post-build.sh ../device/rockchip/common/post-build.sh; do
 	sudo $script "$(realpath "$TARGET_ROOTFS_DIR")"
 done
 
-EXTRA_SIZE_MB=300
-IMAGE_SIZE_MB=$(( $(sudo du -sh -m ${TARGET_ROOTFS_DIR} | cut -f1) + ${EXTRA_SIZE_MB} ))
+# Apparent size + maxium alignment(file_count * block_size) + maxium journal size
+IMAGE_SIZE_MB=$(( $(sudo du --apparent-size -sm ${TARGET_ROOTFS_DIR} | cut -f1) + \
+	$(sudo find ${TARGET_ROOTFS_DIR} | wc -l) * 4 / 1024 + 64 ))
+
+# Extra 10%
+IMAGE_SIZE_MB=$(( $IMAGE_SIZE_MB * 110 / 100 ))
+
 sudo mkfs.ext4 -d ${TARGET_ROOTFS_DIR} ${ROOTFSIMAGE} ${IMAGE_SIZE_MB}M
 
 echo Rootfs Image: ${ROOTFSIMAGE}
