186a646a41ee7710122f24ddbc459d933233c2a1
Caesar Wang <wxt@rock-chips.com>
2024-06-17 10:39:04 +0800

    overlay: remove unused Powermanager
    That will do on post rootfs
    
    Change-Id: I9c715b17ef2721f56c66a77b2efbac5660ef800e
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- overlay/etc/init.d/rockchip.sh
+++ overlay/etc/init.d/rockchip.sh
@@ -163,19 +163,14 @@ fi
 # support power management
 if [ -e "/usr/sbin/pm-suspend" -a -e /etc/Powermanager ] ;
 then
-    mv /etc/Powermanager/power-key.sh /usr/bin/
-    mv /etc/Powermanager/power-key.conf /etc/triggerhappy/triggers.d/
     if [[ "$CHIPNAME" == "rk3399pro" ]];
     then
         mv /etc/Powermanager/01npu /usr/lib/pm-utils/sleep.d/
         mv /etc/Powermanager/02npu /lib/systemd/system-sleep/
+        service triggerhappy restart
     fi
-    mv /etc/Powermanager/03wifibt /usr/lib/pm-utils/sleep.d/
-    mv /etc/Powermanager/04wifibt /lib/systemd/system-sleep/
-    mv /etc/Powermanager/triggerhappy /etc/init.d/triggerhappy
 
     rm /etc/Powermanager -rf
-    service triggerhappy restart
 fi
 
 # Create dummy video node for chromium V4L2 VDA/VEA with rkmpp plugin
